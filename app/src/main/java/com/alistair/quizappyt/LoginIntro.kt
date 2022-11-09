f41e71b79dca44c5e6e7a4abf178ed76b3b3c87d
Alistair <alistairm2022@gmail.com>
2022-11-09 21:55:48 +0530

    Add login register logic

--- app/src/main/java/com/alistair/quizappyt/LoginIntro.kt
+++ app/src/main/java/com/alistair/quizappyt/LoginIntro.kt
@@ -1,11 +1,37 @@
 package com.alistair.quizappyt
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.alistair.quizappyt.databinding.ActivityLoginIntroBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginIntro : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginIntroBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login_intro)
+        binding = ActivityLoginIntroBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+val auth = FirebaseAuth.getInstance()
+        if(auth.currentUser!=null){
+            Toast.makeText(this, "User is already logged in", Toast.LENGTH_SHORT).show()
+            redirect("MAIN")
+        }
+
+        binding.btnGetStarted.setOnClickListener {
+            redirect("LOGIN")
+        }
+    }
+
+
+    private fun redirect(name: String) {
+        val intent = when (name) {
+            "LOGIN" -> Intent(this, LoginActivity::class.java)
+            "MAIN" -> Intent(this, MainActivity::class.java)
+            else -> throw Exception("No Path Exists")
+        }
+        startActivity(intent)
+        finish()
     }
 }
--- app/src/main/java/com/alistair/quizappyt/RegisterActivity.kt
+++ app/src/main/java/com/alistair/quizappyt/RegisterActivity.kt
@@ -1,11 +1,58 @@
 package com.alistair.quizappyt
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.alistair.quizappyt.databinding.ActivityRegisterBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class RegisterActivity : AppCompatActivity() {
+    private lateinit var firebaseAuth: FirebaseAuth
+
+    private lateinit var binding: ActivityRegisterBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        binding.btnSignup.setOnClickListener {
+            registerUser()
+        }
+        binding.btnLogin.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+
+    private fun registerUser() {
+        val email = binding.etEmailAddress.text.toString()
+        val password = binding.etPassword.text.toString()
+        val confirmPassword = binding.etConfirmPassword.text.toString()
+
+        if (email.isBlank() || password.isBlank() || confirmPassword.isBlank()) {
+            Toast.makeText(this, "Please enter your email and password", Toast.LENGTH_SHORT).show()
+            return
+        }
+        if (password != confirmPassword) {
+            Toast.makeText(this, "Passwords don't match, please try again", Toast.LENGTH_SHORT)
+                .show()
+            return
+        }
+
+        firebaseAuth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener {
+                if (it.isSuccessful) {
+                    Toast.makeText(this, "Register Successful", Toast.LENGTH_SHORT).show()
+                    val intent = Intent(this, MainActivity::class.java)
+                    startActivity(intent)
+                    finish()
+                } else {
+                    Toast.makeText(this, "Error creating user", Toast.LENGTH_SHORT).show()
+                }
+            }
     }
 }
